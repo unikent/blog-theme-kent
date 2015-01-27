6b93ffdf547b60eb5ad4392dfedfdb6fd84d6208
gjmh-unikent <g.j.m.haddow@kent.ac.uk>
2015-01-27 17:07:19 +0000

    major css changes subheading support featured image support post footer meta

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,13 +2,31 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php $sub = get_post_meta($post->ID, 'SubHeading',true);
+      if(!empty($sub)){
+        ?>
+        <h2><?php echo $sub; ?></h2>
+      <?php
+      }
+      ?>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
+    <?php $fid = get_post_thumbnail_id();
+    if(!empty($fid)){
+      $fsrc = wp_get_attachment_image_src($fid,'large',false);
+      $fsrc = $fsrc[0];
+      ?>
+      <div class="entry-featured">
+        <img src="<?php echo $fsrc; ?>" class="featured-img">
+      </div>
+    <?php
+    }
+    ?>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
     <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+      <?php get_template_part('templates/entry-footer-meta'); ?>
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
   </article>
---
+++ templates/entry-footer-meta.php
@@ -0,0 +1,20 @@
+<?php
+$categories_list = get_the_category_list( ', ');
+if ( $categories_list ) {
+?>
+<span class="cat-links"><span class="screen-reader-text">Categories: </span><?php echo $categories_list; ?></span>
+<?php
+}
+
+$tags_list = get_the_tag_list( '',', ');
+if ( $tags_list ) {
+    ?>
+    <span class="tag-links"><span class="screen-reader-text">Tags: </span><?php echo $tags_list; ?></span>
+<?php
+}
+
+if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+echo '<span class="comments-link">';
+		comments_popup_link( 'Leave a comment', '1 Comment', '% Comments');
+		echo '</span>';
+}
